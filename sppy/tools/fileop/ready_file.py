b5f8ff0ea61566b311ec0863203c4b673fb1b3a2
zzeppozz <zzeppozz@gmail.com>
2023-04-11 11:03:14 -0500

    bug and pre-commit fixes

--- sppy/tools/fileop/ready_file.py
+++ sppy/tools/fileop/ready_file.py
@@ -1,30 +1,39 @@
-"""Module containing file functions"""
+"""Module containing file functions."""
 
 import glob
 import os
-import requests
 
-SHP_EXT = 'shp'
+SHP_EXT = "shp"
 SHP_EXTENSIONS = [
-    '.shp', '.shx', '.dbf', '.prj', '.sbn', '.sbx', '.fbn', '.fbx', '.ain',
-    '.aih', '.ixs', '.mxs', '.atx', '.shp.xml', '.cpg', '.qix'],
+    ".shp", ".shx", ".dbf", ".prj", ".sbn", ".sbx", ".fbn", ".fbx", ".ain",
+    ".aih", ".ixs", ".mxs", ".atx", ".shp.xml", ".cpg", ".qix"],
 
 
 # ...............................................
 def ready_filename(full_filename, overwrite=False):
-    """Prepare the specified file location for writing."""
+    """Prepare the specified file location for writing.
+
+    Args:
+        full_filename: Full path to filename to check.
+        overwrite: True to delete the full_filename if it exists.
+
+    Returns:
+        boolean flag, true if ready to write, false if not.
+
+    Raises:
+        Exception: on filename to check is None.
+        Exception: on parent directories do not exist and cannot be created.
+    """
     if full_filename is None:
-        raise Exception('Full filename is None')
+        raise Exception("full_filename is `None`")
 
     if os.path.exists(full_filename):
         if overwrite:
             success, msg = delete_file(full_filename)
             if not success:
-                raise Exception('Unable to delete {}: {}'.format(
+                raise Exception("Unable to delete {}: {}".format(
                     full_filename, msg))
-
             return True
-
         return False
 
     pth, _ = os.path.split(full_filename)
@@ -37,27 +46,34 @@ def ready_filename(full_filename, overwrite=False):
         return True
 
     raise Exception(
-        'Failed to create dirs {}, checking for ready_filename {}'.format(
-            pth, full_filename))
+        f"Failed to create dirs {pth}, checking for ready_filename {full_filename}")
 
 
 # ...............................................
-def delete_file(file_name, delete_dir=False):
-    """Delete file if it exists, delete directory if it becomes empty
+def delete_file(full_filename, delete_dir=False):
+    """Delete file if it exists, delete directory if it becomes empty.
+
+    Args:
+        full_filename: Full path to filename to check.
+        delete_dir: True to delete the parent directory if empty after file deletion.
+
+    Returns:
+        success: True on successful deletion, False on failure.
+        msg: An error message if success is False
 
     Note:
         If file is shapefile, delete all related files
     """
     success = True
-    msg = ''
-    if file_name is None:
-        msg = 'Cannot delete file \'None\''
+    msg = ""
+    if full_filename is None:
+        msg = "Cannot delete file `None`"
     else:
-        pth, _ = os.path.split(file_name)
-        if file_name is not None and os.path.exists(file_name):
-            base, ext = os.path.splitext(file_name)
+        pth, _ = os.path.split(full_filename)
+        if full_filename is not None and os.path.exists(full_filename):
+            base, ext = os.path.splitext(full_filename)
             if ext == SHP_EXT:
-                similar_file_names = glob.glob(base + '.*')
+                similar_file_names = glob.glob(f"{base}.*")
                 try:
                     for sim_file_name in similar_file_names:
                         _, sim_ext = os.path.splitext(sim_file_name)
@@ -65,20 +81,19 @@ def delete_file(file_name, delete_dir=False):
                             os.remove(sim_file_name)
                 except Exception as e:
                     success = False
-                    msg = 'Failed to remove {}, {}'.format(
-                        sim_file_name, str(e))
+                    msg = f"Failed to remove {sim_file_name}, {e}"
             else:
                 try:
-                    os.remove(file_name)
+                    os.remove(full_filename)
                 except Exception as e:
                     success = False
-                    msg = 'Failed to remove {}, {}'.format(file_name, str(e))
+                    msg = f"Failed to remove {full_filename}, {e}"
             if delete_dir and len(os.listdir(pth)) == 0:
                 try:
                     os.removedirs(pth)
                 except Exception as e:
                     success = False
-                    msg = 'Failed to remove {}, {}'.format(pth, str(e))
+                    msg = f"Failed to remove {pth}, {e}"
     return success, msg
 
 
@@ -88,19 +103,19 @@ def delete_file(file_name, delete_dir=False):
 #
 #     Args:
 #         base_name: (optional) If provided, this will be the prefix for the
-#             names of the shape file's files in the zip file.
+#             names of the shape file"s files in the zip file.
 #     """
 #     tg_stream = StringIO()
 #     zipf = zipfile.ZipFile(
-#         tg_stream, mode='w', compression=zipfile.ZIP_DEFLATED,
+#         tg_stream, mode="w", compression=zipfile.ZIP_DEFLATED,
 #         allowZip64=True)
 #
 #     for fname in fnames:
 #         ext = os.path.splitext(fname)[1]
-#         zipf.write(fname, '{}.{}'.format(zip_fname, ext))
+#         zipf.write(fname, "{}.{}".format(zip_fname, ext))
 #     zipf.close()
 #
 #     tg_stream.seek(0)
-#     ret = ''.join(tg_stream.readlines())
+#     ret = "".join(tg_stream.readlines())
 #     tg_stream.close()
 #     return ret
---
+++ sppy/tools/fileop/split_records.py
@@ -0,0 +1,341 @@
+"""Class to split a CSV file of records into files grouped by a value in one field."""
+from logging import ERROR
+import os
+
+from sppy.tools.util.logtools import Logger
+from sppy.tools.util.fileop import get_csv_reader, get_csv_writer
+
+ENCODING = "utf-8"
+
+
+# ...............................................
+def usage():
+    """Print a usage help message."""
+    output = """
+    Usage:
+        gbifsort infile [split | sort | merge | check]
+    """
+    print(output)
+    exit(-1)
+
+
+# ..........................................................................
+class DataSplitter(object):
+    """Class to split CSV files by a value."""
+
+    # ...............................................
+    def __init__(self, infname, indelimiter, group_col, logname):
+        """Split a large CSV file into individual files grouped by one column.
+
+        Args:
+            infname: full pathname to a CSV file containing records to be
+                grouped on the value in a field of the records
+            indelimiter: separator between fields of a record
+            group_col: the column name (for files with a header) or column
+                index for the field to be used for grouping
+            logname: the basename for a message logger
+
+        Raises:
+            Exception: on group_col is not in the input file header.
+        """
+        self.messyfile = infname
+        self._basepath, fname = os.path.split(self.messyfile)
+        self._dataname = os.path.basename(fname)
+        self._log = Logger(logname, log_path=self._basepath, log_console=True)
+
+        self.indelimiter = indelimiter
+        self.group_col = group_col
+        self.header = self._get_header()
+
+        self.group_idx = None
+        try:
+            self.group_idx = int(group_col)
+        except ValueError:
+            try:
+                self.group_idx = self.header.index(group_col)
+            except ValueError:
+                raise Exception(
+                    f"Field {self.group_col} does not exist in header {self.header}")
+        # New output files, named by and grouped on group_col
+        self._files = {}
+
+    # ...............................................
+    def close(self, fname=None):
+        """Close one or more output files.
+
+        Args:
+            fname: single file to close.
+        """
+        if fname is not None:
+            self._files[fname].close()
+            f = {}
+            f.pop(fname)
+        else:
+            for f in self._files.values():
+                f.close()
+            self._files = {}
+
+    # ...............................................
+    def _open_group_file(self, grpval, out_delimiter):
+        basefname = f"{self._dataname}_{grpval}.csv"
+        grp_fname = os.path.join(self._basepath, basefname)
+        writer, outf = get_csv_writer(grp_fname, out_delimiter, ENCODING)
+        writer.writerow(self.header)
+        self._files[grp_fname] = outf
+        return writer
+
+    # ...............................................
+    def gather_groupvals(self, fname):
+        """Read a CSV file, and track unique group values and the record count for each.
+
+        Args:
+            fname: Input data file
+
+        Raises:
+            Exception: on failure to get CSVReader.
+        """
+        try:
+            reader, inf = get_csv_reader(self.messyfile, self.indelimiter, ENCODING)
+        except Exception:
+            raise
+        else:
+            groups = {}
+
+            grpval = None
+            grpcount = 0
+            for row in reader:
+                try:
+                    currval = row[self.group_idx]
+                except Exception:
+                    self._log.log(
+                        f"Failed to get column {self.group_idx} from record "
+                        f"{reader.line_num}", refname=self.__class__.__name__,
+                        log_level=ERROR
+                    )
+                else:
+                    if grpval is None:
+                        grpval = currval
+                    if currval != grpval:
+                        self._log.log(
+                            f"Start new group {currval} on record {reader.line_num}",
+                            refname=self.__class__.__name__, log_level=ERROR
+                        )
+                        try:
+                            groups[grpval] += grpcount
+                        except KeyError:
+                            groups[grpval] = grpcount
+                        grpcount = 1
+                        grpval = currval
+                    else:
+                        grpcount += 1
+            inf.close()
+
+        try:
+            writer, outf = get_csv_writer(fname, self.indelimiter, ENCODING)
+        except Exception:
+            raise
+        else:
+            writer.writerow(["groupvalue", "count"])
+            try:
+                for grpval, grpcount in groups.items():
+                    writer.writerow([grpval, grpcount])
+            except Exception:
+                self._log.log(
+                    f"Failed to write record {[grpval, grpcount]} at line"
+                    f" {writer.line_num}", refname=self.__class__.__name__,
+                    log_level=ERROR
+                )
+            finally:
+                outf.close()
+
+    # ...............................................
+    def write_group_files(self, out_delimiter):
+        """Split large file into smaller files with records of a single group value.
+
+        Args:
+            out_delimiter: field delimiter for output files.
+
+        Raises:
+            Exception: on failure to get CSVReader.
+
+        Note:
+            * The number of group files must be small enough for the system to
+                have them all open at the same time.
+            * Use `gather` to evaluate the dataset first.
+        """
+        try:
+            reader, inf = get_csv_reader(self.messyfile, self.indelimiter, ENCODING)
+        except Exception:
+            raise
+        else:
+            # Open infile
+            self._files[self.messyfile] = inf
+            header = next(reader)
+            if self.group_idx is None:
+                try:
+                    self.group_idx = header.index(self.group_col)
+                except ValueError:
+                    raise Exception(
+                        f"Field {self.group_col} does not exist in header {header}"
+                    )
+
+            # {groupval: csvwriter}
+            groupfiles = {}
+            # Read/write each record to a new or existing groupfile
+            for row in reader:
+                try:
+                    grpval = row[self.group_idx]
+                except Exception:
+                    self._log.log(
+                        f"Failed to get column {self.group_idx} from record "
+                        f"{reader.line_num}", refname=self.__class__.__name__,
+                        log_level=ERROR
+                    )
+                else:
+                    try:
+                        wtr = groupfiles[grpval]
+                    except KeyError:
+                        wtr = self._open_group_file(grpval, out_delimiter)
+                        groupfiles[grpval] = wtr
+                        wtr.writerow(header)
+
+                    wtr.writerow(row)
+            # Close all files
+            self.close()
+
+    # ...............................................
+    def _get_header(self):
+        reader, inf = get_csv_reader(self.messyfile, self.indelimiter, ENCODING)
+        header = next(reader)
+        inf.close()
+        return header
+
+    # # ...............................................
+    # def _read_sortvals(self, group_cols):
+    #     self._log.log(
+    #         f"Gathering unique sort values from file {self.messyfile}",
+    #         refname=self.__class__.__name__
+    #     )
+    #     reader, inf = get_csv_reader(self.messyfile, self.indelimiter, ENCODING)
+    #
+    #     group_idxs = self._get_sortidxs(reader, group_cols)
+    #     sortvals = set()
+    #     try:
+    #         for row in reader:
+    #             vals = []
+    #             for idx in group_idxs:
+    #                 vals.append(row[idx])
+    #             sortvals.add(tuple(vals))
+    #     except Exception as e:
+    #         self._log.log(
+    #             f"Exception reading infile {self.messyfile}: {e}",
+    #             refname=self.__class__.__name__, log_level=ERROR
+    #         )
+    #     finally:
+    #         inf.close()
+    #
+    #     self._log.info(
+    #         f"File contained {len(sortvals)} unique sort values",
+    #         refname=self.__class__.__name__, log_level=ERROR
+    #     )
+    #     return sortvals
+
+    # ...............................................
+    def test(self, test_fname, outdelimiter):
+        """Test merged/sorted file.
+
+        Args:
+            test_fname: input file for testing.
+            outdelimiter: field delimiter for input file.
+        """
+        self._log.log(f"Testing file {test_fname}", refname=self.__class__.__name__)
+        reccount = 0
+        reader, outf = get_csv_reader(test_fname, outdelimiter, ENCODING)
+        header = next(reader)
+        if header[self.group_idx] != "gbifID":
+            self._log.log(
+                f"Bad header in {test_fname}", refname=self.__class__.__name__,
+                log_level=ERROR
+            )
+
+        currid = 0
+        for row in reader:
+            reccount += 1
+            try:
+                gbifid = int(row[self.group_idx])
+            except IndexError:
+                self._log.log(
+                    f"Group index {self.group_idx} not present in {len(row)} fields of "
+                    f"rec {reader.line_num}",
+                    refname=self.__class__.__name__, log_level=ERROR
+                )
+            except ValueError:
+                self._log.log(
+                    f"Bad gbifID on rec {reader.line_num}",
+                    refname=self.__class__.__name__, log_level=ERROR
+                )
+            else:
+                if gbifid < currid:
+                    self._log.log(
+                        f"Bad sort gbifID {gbifid} on rec {reader.line_num}",
+                        refname=self.__class__.__name__, log_level=ERROR
+                    )
+                    break
+                elif gbifid == currid:
+                    self._log.log(
+                        f"Duplicate gbifID {gbifid} on rec {reader.line_num}",
+                        refname=self.__class__.__name__, log_level=ERROR
+                    )
+                else:
+                    currid = gbifid
+
+        self._log.log(
+            f"File contained {reccount} records", refname=self.__class__.__name__)
+        outf.close()
+
+
+# .............................................................................
+if __name__ == "__main__":
+    # inputFilename, delimiter, group_index, logname)
+    import argparse
+    parser = argparse.ArgumentParser(
+        description=(
+            "Group CSV dataset, optionally into separate files, on a given field")
+    )
+    parser.add_argument(
+        "infile", type=str,
+        help="Absolute pathname of the input delimited text file"
+    )
+    parser.add_argument(
+        "--input_delimiter", type=str, default="$",
+        help="Delimiter between fields for input file"
+    )
+    parser.add_argument(
+        "--output_delimiter", type=str, default="$",
+        help="Delimiter between fields for output file(s)"
+    )
+    parser.add_argument(
+        "--group_column", type=str, default="resource_id",
+        help="Index or column name of field for data grouping"
+    )
+    args = parser.parse_args()
+    unsorted_file = args.infile
+    in_delimiter = args.input_delimiter
+    out_delimiter = args.output_delimiter
+    group_col = args.group_column
+
+    if not os.path.exists(unsorted_file):
+        print(f"Input CSV file {unsorted_file} does not exist")
+    else:
+        scriptname, ext = os.path.splitext(os.path.basename(__file__))
+
+        pth, fname = os.path.split(unsorted_file)
+        dataname, ext = os.path.splitext(fname)
+        logname = f"{scriptname}_{dataname}"
+
+        gf = DataSplitter(unsorted_file, in_delimiter, group_col, logname)
+
+        try:
+            gf.write_group_files(out_delimiter)
+        finally:
+            gf.close()
