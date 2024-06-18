21cb4de75059a46b1f13882a9a8d1f5c93610281
Aimee Stewart <zzeppozz@gmail.com>
2024-06-18 16:08:20 -0500

    more error checking; download to environ variable WORKING_DIRECTORY

--- flask_app/analyst/dataset.py
+++ flask_app/analyst/dataset.py
@@ -1,5 +1,6 @@
 """Class for the Specify Network Name API service."""
 from http import HTTPStatus
+import os
 from werkzeug.exceptions import BadRequest
 
 from flask_app.common.s2n_type import APIService, AnalystOutput
@@ -12,8 +13,10 @@ from sppy.aws.aggregate_matrix import SparseMatrix
 from sppy.aws.aws_tools import (
     download_from_s3, get_current_datadate_str
 )
-from sppy.tools.s2n.utils import (combine_errinfo, get_traceback, prettify_object)
+from sppy.tools.s2n.utils import (
+    add_errinfo, combine_errinfo, get_traceback, prettify_object)
 
+LOCAL_PATH = os.environ["WORKING_DIRECTORY"]
 
 # .............................................................................
 class DatasetSvc(_AnalystService):
@@ -61,10 +64,9 @@ class DatasetSvc(_AnalystService):
                     good_params["dataset_key"], good_params["species_key"],
                     good_params["aggregate_by"], good_params["stat_type"])
             except Exception:
-                errors = {"error": [get_traceback()]}
-
-            # Combine errors from success or failure
-            errinfo = combine_errinfo(errinfo, errors)
+                errinfo = add_errinfo(errinfo, "error", get_traceback())
+            else:
+                errinfo = combine_errinfo(errinfo, errors)
 
         # Assemble
         full_out = AnalystOutput(
@@ -76,79 +78,92 @@ class DatasetSvc(_AnalystService):
     # ...............................................
     @classmethod
     def _init_sparse_matrix(cls):
+        errinfo = {}
+        sp_mtx = None
         data_datestr = get_current_datadate_str()
         mtx_table_type = SUMMARY_TABLE_TYPES.SPECIES_DATASET_MATRIX
-        local_path = "/tmp"
         table = Summaries.get_table(mtx_table_type, data_datestr)
         zip_fname = f"{table['fname']}.zip"
         # Only download if file does not exist
-        zip_filename = download_from_s3(
-            PROJ_BUCKET, SUMMARY_FOLDER, zip_fname, local_path=local_path,
-            overwrite=False)
-        # Only extract if files do not exist
-        sparse_coo, row_categ, col_categ, table_type, _data_datestr = \
-            SparseMatrix.uncompress_zipped_sparsematrix(
-                zip_filename, local_path=local_path, overwrite=False)
-        # Create
-        sp_mtx = SparseMatrix(
-            sparse_coo, mtx_table_type, data_datestr, row_category=row_categ,
-            column_category=col_categ, logger=None)
-        return sp_mtx
+        try:
+            zip_filename = download_from_s3(
+                PROJ_BUCKET, SUMMARY_FOLDER, zip_fname, local_path=LOCAL_PATH,
+                overwrite=False)
+        except Exception as e:
+            errinfo["error"] = [e]
+        else:
+            # Only extract if files do not exist
+            try:
+                sparse_coo, row_categ, col_categ, table_type, _data_datestr = \
+                    SparseMatrix.uncompress_zipped_sparsematrix(
+                        zip_filename, local_path=LOCAL_PATH, overwrite=False)
+            except Exception as e:
+                errinfo = add_errinfo(errinfo, "error", e)
+            # Create
+            else:
+                sp_mtx = SparseMatrix(
+                    sparse_coo, mtx_table_type, data_datestr, row_category=row_categ,
+                    column_category=col_categ, logger=None)
+        return sp_mtx, errinfo
 
     # ...............................................
     @classmethod
     def _get_speciesxdataset_counts(
             cls, dataset_key, species_key, aggregate_by, stat_type):
         stat_dict = {}
-        errors = {}
         ds_stat_dict = sp_stat_dict = None
-        spnet_mtx = cls._init_sparse_matrix()
-        if aggregate_by is None:
-            agg_type = aggregate_by
-        elif aggregate_by == "occurrences":
-            agg_type = "value"
-        elif aggregate_by in ("species", "dataset"):
-            agg_type = "axis"
+        spnet_mtx, errinfo = cls._init_sparse_matrix()
+        if spnet_mtx is not None:
+            if aggregate_by is None:
+                agg_type = aggregate_by
+            elif aggregate_by == "occurrences":
+                agg_type = "value"
+            elif aggregate_by in ("species", "dataset"):
+                agg_type = "axis"
 
-        if dataset_key is not None:
-            if stat_type == "describe":
-                try:
-                    ds_stat_dict = spnet_mtx.get_column_stats(
-                        dataset_key, agg_type=agg_type)
-                except Exception:
-                    traceback = get_traceback()
-                    errors["error"] = [HTTPStatus.INTERNAL_SERVER_ERROR, traceback]
-            else:
-                # if compare
-                try:
-                    ds_stat_dict = spnet_mtx.compare_column_to_others(
-                        dataset_key, agg_type=agg_type)
-                except Exception:
-                    traceback = get_traceback()
-                    errors["error"] = [HTTPStatus.INTERNAL_SERVER_ERROR, traceback]
+            if dataset_key is not None:
+                if stat_type == "describe":
+                    try:
+                        ds_stat_dict = spnet_mtx.get_column_stats(
+                            dataset_key, agg_type=agg_type)
+                    except Exception:
+                        errinfo = {
+                            "error": [HTTPStatus.INTERNAL_SERVER_ERROR, get_traceback()]
+                        }
+                else:
+                    # if compare
+                    try:
+                        ds_stat_dict = spnet_mtx.compare_column_to_others(
+                            dataset_key, agg_type=agg_type)
+                    except Exception:
+                        errinfo = add_errinfo(
+                            errinfo, "error",
+                            [HTTPStatus.INTERNAL_SERVER_ERROR, get_traceback()])
 
-        if species_key is not None:
-            if stat_type == "describe":
-                try:
-                    sp_stat_dict = spnet_mtx.get_row_stats(species_key)
-                except Exception:
-                    traceback = get_traceback()
-                    errors["error"] = [HTTPStatus.INTERNAL_SERVER_ERROR, traceback]
-            else:
-                # if compare
-                try:
-                    sp_stat_dict = spnet_mtx.compare_row_to_others(
-                        species_key, agg_type=agg_type)
-                except Exception:
-                    traceback = get_traceback()
-                    errors["error"] = [HTTPStatus.INTERNAL_SERVER_ERROR, traceback]
+            if species_key is not None:
+                if stat_type == "describe":
+                    try:
+                        sp_stat_dict = spnet_mtx.get_row_stats(species_key)
+                    except Exception:
+                        errinfo = add_errinfo(
+                            errinfo, "error",
+                            [HTTPStatus.INTERNAL_SERVER_ERROR, get_traceback()])
+                else:
+                    # if compare
+                    try:
+                        sp_stat_dict = spnet_mtx.compare_row_to_others(
+                            species_key, agg_type=agg_type)
+                    except Exception:
+                        errinfo = add_errinfo(
+                            errinfo, "error",
+                            [HTTPStatus.INTERNAL_SERVER_ERROR, get_traceback()])
 
-        if ds_stat_dict is not None:
-            stat_dict["Dataset Statistics"] = ds_stat_dict
-        if sp_stat_dict is not None:
-            stat_dict["Species Statistics"] = sp_stat_dict
+            if ds_stat_dict is not None:
+                stat_dict["Dataset Statistics"] = ds_stat_dict
+            if sp_stat_dict is not None:
+                stat_dict["Species Statistics"] = sp_stat_dict
 
-        return stat_dict, errors
+        return stat_dict, errinfo
 
 
 # .............................................................................
@@ -159,15 +174,47 @@ if __name__ == "__main__":
 
     svc = DatasetSvc()
     response = svc.get_endpoint()
-    # print("print_output:")
-    # AnalystOutput.print_output(response, do_print_rec=True)
-    # print("prettify:")
     print(prettify_object(response))
 
     response = svc.get_dataset_counts(
         dataset_key=dataset_key, species_key=key_species, stat_type="compare",
         aggregate_by=agg_type)
-    # print("print_output:")
-    # AnalystOutput.print_output(response, do_print_rec=True)
-    # print("prettify:")
     print(prettify_object(response))
+
+"""
+from flask_app.analyst.dataset import *
+
+dataset_key = "3e2d26d9-2776-4bec-bdc7-bab3842ffb6b"
+key_species = "11378306 Phaneroptera laticerca"
+agg_type = None
+
+svc = DatasetSvc()
+response = svc.get_endpoint()
+print(prettify_object(response))
+
+response = svc.get_dataset_counts(
+    dataset_key=dataset_key, species_key=key_species, stat_type="compare",
+    aggregate_by=agg_type)
+print(prettify_object(response))
+
+# ...................................
+
+data_datestr = get_current_datadate_str()
+mtx_table_type = SUMMARY_TABLE_TYPES.SPECIES_DATASET_MATRIX
+local_path = "/tmp"
+table = Summaries.get_table(mtx_table_type, data_datestr)
+zip_fname = f"{table['fname']}.zip"
+# Only download if file does not exist
+zip_filename = download_from_s3(
+    PROJ_BUCKET, SUMMARY_FOLDER, zip_fname, local_path=local_path,
+    overwrite=False)
+# Only extract if files do not exist
+sparse_coo, row_categ, col_categ, table_type, _data_datestr = \
+    SparseMatrix.uncompress_zipped_sparsematrix(
+        zip_filename, local_path=local_path, overwrite=False)
+# Create
+sp_mtx = SparseMatrix(
+    sparse_coo, mtx_table_type, data_datestr, row_category=row_categ,
+    column_category=col_categ, logger=None)
+
+"""
