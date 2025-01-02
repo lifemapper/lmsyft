d19412c9b407afdb89d5a3dd57b03eddbab7ffd8
Aimee Stewart <zzeppozz@gmail.com>
2025-01-02 16:27:31 -0600

    test with branch

--- specnet/task/calc_stats.userdata.sh
+++ specnet/task/calc_stats.userdata.sh
@@ -2,7 +2,7 @@
 sudo apt-get -y update
 sudo apt-get -y install docker.io
 sudo apt-get -y install docker-compose-v2
-git clone https://github.com/specifysystems/sp_network.git
+git clone --branch 107-automate-AWS-data-workflow https://github.com/specifysystems/sp_network.git
 cd sp_network
 sudo docker compose -f compose.calc_stats.yml up
 sudo shutdown -h now
